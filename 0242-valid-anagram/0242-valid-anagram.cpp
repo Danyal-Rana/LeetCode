2a4090a92d8a780e1b16121d06f88387c7935df4
Muhammad Danyal Rana <100080102+Danyal-Rana@users.noreply.github.com>
2023-09-16 21:07:09 +0500

    Time: 5 ms (82.91%), Space: 7.7 MB (5.23%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,9 +2,31 @@ class Solution {
 public:
     bool isAnagram(string s, string t)
     {
-        sort (s.begin(), s.end());
-        sort (t.begin(), t.end());
+        if (s.size() != t.size())
+        {
+            return false;
+        }
 
-        return s==t;
+        int sArray[26] = {0};
+        int tArray[26] = {0};
+
+        for (int i=0; i<s.size(); i++)
+        {
+            sArray[s[i]-'a']++;
+        }
+
+        for (int i=0; i<t.size(); i++)
+        {
+            tArray[t[i]-'a']++;
+        }
+
+        for (int i=0; i<26; i++)
+        {
+            if (sArray[i] != tArray[i])
+            {
+                return false;
+            }
+        }
+        return true;
     }
 };
