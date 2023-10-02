fa10080a5f70fb7e385180661906b87c28883ddf
Muhammad Danyal Rana <100080102+Danyal-Rana@users.noreply.github.com>
2023-10-02 15:52:09 +0500

    Time: 0 ms (100%), Space: 10 MB (19.67%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.cpp
+++ 1580-shuffle-the-array/1580-shuffle-the-array.cpp
@@ -6,9 +6,10 @@ public:
 
         for (int i=0; i<n; i++)
         {
-            myVector.push_back(nums[i]);
-            myVector.push_back(nums[i+n]);
+            myVector.push_back (nums[i]);
+            myVector.push_back (nums[i+n]);
         }
+
         return myVector;
     }
 };
