6d11007568d07548d13ba918125a227db7fc46f8
Muhammad Danyal Rana <100080102+Danyal-Rana@users.noreply.github.com>
2023-10-06 05:39:19 -0700

    Time: 194 ms (54.57%), Space: 95.1 MB (48.16%) - LeetHub

---
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums)
+    {
+        unordered_map <int, int> myMap;
+
+        int minE = *min_element (begin(nums), end(nums));
+        int maxE = *max_element (begin(nums), end(nums));
+
+        for (int &num : nums)
+        {
+            myMap[num]++;
+        }
+
+        int i = 0;
+
+        for (int j=minE; j<=maxE; j++)
+        {
+            while (myMap[j] > 0)
+            {
+                nums[i] = j;
+                myMap[j]--;
+                i++;
+            }
+        }
+
+        return nums;
+    }
+};
