142942202ac063c9a1f37b671f39bbbe4a2e32ff
Muhammad Danyal Rana <100080102+Danyal-Rana@users.noreply.github.com>
2023-12-22 19:16:31 +0500

    Time: 3 ms (50.89%), Space: 6.6 MB (62.95%) - LeetHub

---
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int maxScore(string s)
+    {
+        int n = s.size();
+        int maxi = 0;
+        
+        for (int i=1; i<n; i++)
+        {
+            int zero = 0;
+            int one = 0;
+            
+            for (int j=i-1; j>=0; j--)
+            {
+                if (s[j]=='0')
+                {
+                    zero++;
+                }
+            }
+            
+            for (int j=i; j<n; j++)
+            {
+                if (s[j]=='1')
+                {
+                    one++;
+                }
+            }
+            
+            if (one+zero > maxi)
+            {
+                maxi = one+zero;
+            }
+        }
+        return maxi;
+        
+    }
+};
