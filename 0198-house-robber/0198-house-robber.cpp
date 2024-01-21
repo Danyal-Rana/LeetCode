187104a67eb93d8d9caa9a12cce2cc545e04239a
Muhammad Danyal Rana <100080102+Danyal-Rana@users.noreply.github.com>
2024-01-21 23:32:22 +0500

    Time: 0 ms (100.00%), Space: 8.9 MB (9.67%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int dp0 = 0, dp1 = 0;
+        for (auto& num : nums) {
+            int curr = max(dp0 + num /* take */, dp1 /* skip (not take) */);
+            dp0 = dp1;
+            dp1 = curr;
+        }
+        return max(dp0, dp1);
+    }
+};
