1489433b1320b8bd64ba7d3c8540d83d734bce7b
Muhammad Danyal Rana <100080102+Danyal-Rana@users.noreply.github.com>
2024-02-05 15:13:52 +0500

    Time: 15 ms (97.21%), Space: 11.7 MB (31.20%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -2,26 +2,21 @@ class Solution {
 public:
     int firstUniqChar(string s)
     {
-       
+        int freq[26] = {0};
+        
+        for (char c : s)
+        {
+            freq[c-'a']++;
+        }
+        
         for (int i=0; i<s.size(); i++)
         {
-            int myCounter = 0;
-            for (int j=0; j<s.size(); j++)
-            {
-                if (s[i] == s[j])
-                {
-                    myCounter++;
-                    if (myCounter > 1)
-                    {
-                        break;
-                    }
-                }
-            }
-            if (myCounter == 1)
+            if (freq[s[i]-'a'] == 1)
             {
                 return i;
             }
         }
-        return -1;        
+        
+        return -1;
     }
 };
