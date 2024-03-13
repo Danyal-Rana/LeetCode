02f102989f8c92f2565c21799398808eb456cda5
Muhammad Danyal Rana <100080102+Danyal-Rana@users.noreply.github.com>
2024-03-13 15:47:14 +0500

    Time: 3 ms (51.39%), Space: 7.2 MB (41.86%) - LeetHub

---
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int pivotInteger(int n)
+    {
+        int sum = n * (n+1)/2;
+        double x = sqrt (sum);
+        
+        if (x - ceil(x) == 0)
+        {
+            return int(x);
+        }
+        else
+        {
+            return -1;
+        }
+    }
+};
