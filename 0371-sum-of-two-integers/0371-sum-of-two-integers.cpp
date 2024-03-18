0775ac5be99248009306947459b4593f60634d02
Muhammad Danyal Rana <100080102+Danyal-Rana@users.noreply.github.com>
2024-03-18 17:00:10 +0500

    Time: 0 ms (100.00%), Space: 7.1 MB (64.85%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -2,6 +2,24 @@ class Solution {
 public:
     int getSum(int a, int b)
     {
-        return a+b;
+        int ans = max (a, b);
+        int temp = min (a, b);
+        
+        if (ans>=0 && temp>=0)
+        {
+            for (int i=0; i<temp; i++)
+            {
+                ans++;
+            }
+        }
+        else
+        {
+            for (int i=temp; i<0; i++)
+            {
+                ans--;
+            }
+        }
+        
+        return ans;
     }
 };
