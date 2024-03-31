9a9df56a83eaf2036a6fba5516a1a1236a8e8a14
Muhammad Danyal Rana <100080102+Danyal-Rana@users.noreply.github.com>
2024-04-01 04:10:52 +0500

    Time: 4 ms (22.45%), Space: 8.1 MB (45.97%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -2,37 +2,21 @@ class Solution {
 public:
     double myPow(double x, int n)
     {
-        long int m = (long int)n;
-        static double ans = 1;
-        if (m == 0)
+        double myResult = 1;
+        
+        int m = abs(n);
+        
+        while (m > 0)
         {
-            return 1;
+            if (m%2 != 0)
+            {
+                myResult *= x;
+            }
+            
+            x *= x;
+            m /= 2;
         }
-
-        if (m>0)
-        {
-            if (m%2 == 0)
-            {
-                ans = myPow(x*x, m/2);
-            }
-            else
-            {
-                ans = x*(myPow(x*x, (m-1)/2));
-            }
-        }
-
-        else if (m<0)
-        {
-            m = -m;
-            if (m%2 == 0)
-            {
-                ans = 1 / myPow(x*x, m/2);
-            }
-            else
-            {
-                ans = 1 / (x*myPow(x*x, (m-1)/2));
-            }
-        }
-        return ans;
+        
+        return (n>0) ? myResult : 1/myResult;
     }
 };
