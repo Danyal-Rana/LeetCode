741d5499df0926ce39d7a6889bd744b6d79d969b
Muhammad Danyal Rana <100080102+Danyal-Rana@users.noreply.github.com>
2024-04-04 06:47:01 +0500

    Time: 2 ms (40.97%), Space: 7.6 MB (14.40%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -5,21 +5,17 @@ public:
         int ans = 0;
         stack<char> myStack;
 
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] == '(') {
+        for (int i = 0; i < s.size(); i++) 
+        {
+            if (s[i] == '(') 
+            {
                 myStack.push(s[i]);
                 ans = max(ans, (int)myStack.size());
-            } else if (s[i] == ')') {
-                if (!myStack.empty()) {
-                    myStack.pop();
-                } else {
-                    return -1;
-                }
             }
-        }
-        
-        if (!myStack.empty()) {
-            return -1; 
+            else if (s[i] == ')') 
+            {
+                myStack.pop();
+            }
         }
 
         return ans;
