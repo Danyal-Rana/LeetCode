14e52f8796556cc42679b222e83134fd1ba9b065
Muhammad Danyal Rana <100080102+Danyal-Rana@users.noreply.github.com>
2024-05-26 12:41:49 +0500

    Time: 0 ms (100.00%), Space: 7.5 MB (44.21%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.cpp
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.cpp
@@ -2,30 +2,30 @@ class Solution {
 public:
     bool checkRecord(string s)
     {
-        int myAbsent = 0;
-        int myLate = 0;
+        int absentCounter = 0;
+        int lateCounter = 0;
         
         for (auto c : s)
         {
             if (c == 'A')
             {
-                myAbsent++;
-                if (myLate > 0)
-                    myLate = 0;
+                absentCounter++;
+                if (lateCounter > 0)
+                    lateCounter = 0;
             }
             else if (c == 'L')
             {
-                myLate++;
+                lateCounter++;
             }
             else if (c == 'P')
             {
-                if (myLate > 0)
-                    myLate = 0;
+                if (lateCounter > 0)
+                    lateCounter = 0;
                 
                 continue;
             }
             
-            if (myAbsent >= 2 || myLate >= 3)
+            if (absentCounter >= 2 || lateCounter >= 3)
             {
                 return false;
             }
