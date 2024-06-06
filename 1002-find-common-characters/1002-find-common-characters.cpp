ee149405286cdcc6782eb3dfba17083239e82b62
Muhammad Danyal Rana <100080102+Danyal-Rana@users.noreply.github.com>
2024-06-06 10:32:37 +0500

    Time: 11 ms (35.04%), Space: 12 MB (35.97%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.cpp
+++ 1002-find-common-characters/1002-find-common-characters.cpp
@@ -1,21 +1,33 @@
 class Solution {
 public:
-    vector<string> commonChars(vector<string>& a) {
-        vector<int> res(26, INT_MAX);
-
-        for(int i=0;i<a.size();i++) {
-            vector<int> count(26, 0);
-            for(int j=0;j<a[i].size();j++) count[a[i][j]-'a']++;
-            for(int j=0;j<26;j++)res[j]=min(res[j], count[j]);
-        }
-
-        vector<string> resArray;
-        for(int i=0;i<26;i++) {
-            while(res[i]!=0) {
-                resArray.push_back(string(1, 'a' + i));
-                res[i]--;
+    vector<string> commonChars(vector<string>& words)
+    {
+        vector<int> common(26, INT_MAX);
+        vector<string> ans;
+        
+        for (auto s : words)
+        {
+            vector<int> myCounter(26,0);
+            
+            for (auto c : s)
+            {
+                myCounter[c-'a']++;
+            }
+            
+            for (int i=0; i<26; i++)
+            {
+                common[i] = min(common[i], myCounter[i]);
             }
         }
-        return resArray;
+        
+        for (int i=0; i<26; i++)
+        {
+            for (int j=0; j<common[i]; j++)
+            {
+                ans.push_back (string(1, i+'a'));
+            }
+        }
+        
+        return ans;
     }
 };
