d98bad47515241337544637ec5e58516b545982d
Muhammad Danyal Rana <100080102+Danyal-Rana@users.noreply.github.com>
2024-06-10 11:43:37 +0500

    Time: 0 ms (100%), Space: 10.1 MB (78.64%) - LeetHub

---
+++ 1137-height-checker/1137-height-checker.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights)
+    {
+        vector<int> expected = heights;
+        sort(expected.begin(), expected.end());
+        int myCounter = 0;
+
+        for (int i=0; i< heights.size(); i++)
+        {
+            if (expected[i] != heights[i])
+            {
+                myCounter++;
+            }
+        }
+        return myCounter;
+    }
+};
