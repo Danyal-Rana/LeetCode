4759de06c7897eb8e58d242f1e44780ee2d5e5a8
Muhammad Danyal Rana <100080102+Danyal-Rana@users.noreply.github.com>
2024-06-17 16:16:11 +0500

    Time: 0 ms (100%), Space: 7.1 MB (61.35%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c)
+    {
+        for (int divisor=2; divisor*divisor<=c; divisor++)
+        {
+            if (c % divisor == 0)
+            {
+                int exponentCount = 0;
+                while (c%divisor==0)
+                {
+                    exponentCount++;
+                    c /= divisor;
+                }
+
+                if (divisor%4==3 and exponentCount%2 != 0)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return c%4 != 3;        
+    }
+};
