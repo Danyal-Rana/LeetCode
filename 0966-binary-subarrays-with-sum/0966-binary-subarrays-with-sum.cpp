becb36343f38a7e2bccce64c9b74f5eb9c79e008
Muhammad Danyal Rana <100080102+Danyal-Rana@users.noreply.github.com>
2024-06-22 12:49:58 +0500

    Time: 25 ms (50.46%), Space: 31.1 MB (60.99%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
@@ -2,11 +2,6 @@ class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal)
     {
-        if (goal < 0)
-        {
-            return 0;
-        }
-
         int l = 0;
         int r = 0;
         int n = nums.size();
