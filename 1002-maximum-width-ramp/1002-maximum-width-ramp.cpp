a953493979fabb6acd65c219343c03a4f4a65b4b
Muhammad Danyal Rana <100080102+Danyal-Rana@users.noreply.github.com>
2024-10-10 20:50:44 +0500

    Time: 53 ms (44.49%), Space: 47.4 MB (14.29%) - LeetHub

--- 1002-maximum-width-ramp/1002-maximum-width-ramp.cpp
+++ 1002-maximum-width-ramp/1002-maximum-width-ramp.cpp
@@ -7,7 +7,6 @@ public:
         vector<int> rightMax(n);
         rightMax[n-1] = nums[n-1];
 
-        // Fill rightMax to store the maximum value to the right of each element
         for (int i = n - 2; i >= 0; i--)
         {
             rightMax[i] = max(rightMax[i + 1], nums[i]);
@@ -17,10 +16,8 @@ public:
         int i = 0;
         int j = 0;
 
-        // Traverse using two pointers
         while (j < n)
         {
-            // Ensure i doesn't go beyond j and increment it if nums[i] > rightMax[j]
             while (i < j && nums[i] > rightMax[j])
             {
                 i++;
