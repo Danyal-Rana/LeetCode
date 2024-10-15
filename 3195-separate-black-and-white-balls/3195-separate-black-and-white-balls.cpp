7871978263e8b1848ce67ece57349db172c714fe
Muhammad Danyal Rana <100080102+Danyal-Rana@users.noreply.github.com>
2024-10-15 11:03:44 +0500

    Time: 31 ms (73.47%), Space: 15.6 MB (45.68%) - LeetHub

---
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    long long minimumSteps(string s)
+    {
+        long long ans = 0;
+        int oneCount = 0;
+
+        for (int i=0; i<s.size(); i++)
+        {
+            if (s[i]=='1')
+            {
+                oneCount++;
+            }
+            else
+            {
+                ans += oneCount;
+            }
+        }
+
+        return ans;
+    }
+};
