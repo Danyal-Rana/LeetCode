0fc122579bb216539287e3e5c305b649dda08102
Danyal Rana <100080102+Danyal-Rana@users.noreply.github.com>
2024-10-22 04:55:53 +0500

    Time: 18 ms (97.41%), Space: 12.8 MB (94.24%) - LeetHub

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.cpp
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/1715-split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
 
-    void solve(string& s, int idx, unordered_set<string>& st, int currCount, int& maxCount) {
-        if(currCount + (s.length() - idx) <= maxCount) { //Pruning for slight improvement
+    void solve(string& s, int idx, unordered_set<string>& st, int currCount, int& maxCount) 
+    {
+        if(currCount + (s.length() - idx) <= maxCount) 
+        {
             return;
         }
 
-        if(idx == s.length()) {
+        if(idx == s.length()) 
+        {
             maxCount = max(maxCount, currCount);
         }
 
-        for(int j = idx; j < s.length(); j++) {
+        for(int j = idx; j < s.length(); j++) 
+        {
             string sub = s.substr(idx, j-idx+1);
             if(st.find(sub) == st.end()) {
                 st.insert(sub);
@@ -20,7 +24,8 @@ public:
         }
     }
 
-    int maxUniqueSplit(string s) {
+    int maxUniqueSplit(string s) 
+    {
         unordered_set<string> st;
         int maxCount  = 0;
         int currCount = 0;
