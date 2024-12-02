942a7fc26bafe4d0ce05cf80176647912be9813d
Danyal Rana <100080102+Danyal-Rana@users.noreply.github.com>
2024-12-02 21:44:51 +0500

    Time: 2 ms (5.23%), Space: 8.5 MB (5.37%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-    int helper(int n, vector<int> &dp)
-    {
-        if (n < 0)
-            return 0;
-        else if (dp[n] != -1)
-            return dp[n];
-        
-        int singleStep = helper(n-1, dp);
-        int doubleStep = helper(n-2, dp);
-
-        return dp[n]=singleStep+doubleStep;
-    }
-
     int climbStairs(int n)
     {
         vector<int> dp(n+1, -1);
-        dp[0] = 1;
-        return helper(n, dp);        
+        dp[0] = dp[1] = 1;
+
+        for (int i=2; i<=n; i++)
+        {
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+
+        return dp[n];
     }
 };
