67292ed33ec145981f54bf50464160c7fa3a88a6
Danyal Rana <100080102+Danyal-Rana@users.noreply.github.com>
2024-12-02 21:48:58 +0500

    Time: 0 ms (100%), Space: 7.8 MB (9.2%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     int climbStairs(int n)
     {
-        vector<int> dp(n+1, -1);
-        dp[0] = dp[1] = 1;
+        int preOfPre = 1;
+        int pre = 1;
 
         for (int i=2; i<=n; i++)
         {
-            dp[i] = dp[i-1]+dp[i-2];
+            int x = pre+preOfPre;
+            preOfPre = pre;
+            pre = x;
         }
 
-        return dp[n];
+        return pre;
     }
 };
