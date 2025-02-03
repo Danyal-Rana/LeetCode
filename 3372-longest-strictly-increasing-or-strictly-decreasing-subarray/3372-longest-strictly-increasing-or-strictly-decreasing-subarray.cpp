09a66bfa6ce8a83a6ae6a8946353f0c19d604824
Danyal Rana <100080102+Danyal-Rana@users.noreply.github.com>
2025-02-03 12:36:12 +0500

    Time: 0 ms (100%), Space: 27.9 MB (64.71%) - LeetHub

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
-    int longestMonotonicSubarray(vector<int>& nums)
+    int longestMonotonicSubarray(vector<int>& nums) 
     {
-        int maxInc = 0;
-        int maxDec = 0;
-        int tempInc = 0;
-        int tempDec = 0;
+        if (nums.empty()) 
+            return 0;
 
-        for(int i=0; i<nums.size()-1; i++)
-        {
-            if (nums[i] < nums[i+1])
+        int maxInc = 1;
+        int maxDec = 1;
+        int tempInc = 1;
+        int tempDec = 1;
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] > nums[i-1]) 
             {
-                maxDec = max(maxDec, tempDec);
-                tempDec = 0;
                 tempInc++;
-            }
-            else if (nums[i] > nums[i+1])
+                tempDec = 1;
+            } else if (nums[i] < nums[i-1]) 
             {
-                maxInc = max(maxInc, tempInc);
-                tempInc = 0;
                 tempDec++;
+                tempInc = 1;
+            } else {
+                tempInc = 1;
+                tempDec = 1;
             }
-            else
-            {
-                maxInc = max(maxInc, tempInc);
-                maxDec = max(maxDec, tempDec);
-                tempInc = 0;
-                tempDec = 0;
-            }
+
+            maxInc = max(maxInc, tempInc);
+            maxDec = max(maxDec, tempDec);
         }
 
-        maxInc = max(maxInc, tempInc);
-        maxDec = max(maxDec, tempDec);
-
-        return max(maxInc, maxDec)+1;        
+        return max(maxInc, maxDec);
     }
 };
